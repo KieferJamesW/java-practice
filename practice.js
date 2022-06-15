a0b2f16395abd373010dc937d96d325fa8959539
KieferJW <kieferw140@gmail.com>
2022-06-15 10:55:17 -0500

    commit

--- practice.js
+++ practice.js
@@ -15,3 +15,18 @@ for(let i = 1; i <= 10; i++) {
 }
 */
 
+//NOTE: I almost want to come back and try this again as a function and add in a couple more converters but for now I don't know how to do that
+/*let inputFeet = 10
+
+const meters = inputFeet * 0.621371
+
+console.log(meters)
+*/
+
+/*
+const array = [10, 15, -5, 9, 20]
+
+let sum = array.reduce((x, y) => x + y)
+
+console.log(sum)
+*/
